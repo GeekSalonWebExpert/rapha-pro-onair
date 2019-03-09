6b4131ea1a9d884c2bfeee594e298c75b4381c2c
長瀬柊哉 <touya.nagase@ge-ne-sis.com>
2019-03-09 15:49:03 +0900

    App.js と package.jsの追加

---
+++ App.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import './stylesheet';
+
+class App extends Component {
+    constructor() {
+      super()
+      this.state = {
+        tasks: [
+          {
+            id: 1,
+            body: "とりあえず表示してみる"
+          },
+          {
+            id: 2,
+            body: "私たち、いずれ書き換えられる運命"
+          }
+        ]
+      }
+    }
+  
+    render() {
+      return (
+        <div className="App">
+          <div className="tasks">
+          {
+            this.state.tasks.map( task => {
+                return <div className="task" key={ task.id }>{ task.body }</div>
+            })
+          }
+          </div>
+        </div>
+      );
+    }
+  }
+  
+  export default App;
